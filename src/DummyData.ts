40a0d2775295a10a63515c4daa3aa6f26d3a24d1
Ambooon <angoringfrancis29@gmail.com>
2024-07-05 15:40:20 +0800

    Added a temporary dummy data for posts

---
+++ src/DummyData.ts
@@ -0,0 +1,108 @@
+export const data = [
+  {
+    profilePicture:
+      'https://img.freepik.com/free-photo/close-up-young-successful-man-smiling-camera-standing-casual-outfit-against-blue-background_1258-65746.jpg',
+    userID: 'marko_yahoo20',
+    postPicture:
+      'https://images.freeimages.com/variants/mjrbWBMdaBG7WnYHvqQ83M4W/f4a36f6589a0e50e702740b15352bc00e4bfaf6f58bd4db850e167794d05993d?fmt=webp&w=500',
+    caption: 'Unwind',
+  },
+  {
+    profilePicture:
+      'https://img.freepik.com/free-photo/portrait-young-woman-with-natural-make-up_23-2149084942.jpg',
+    userID: 'maryGrace00',
+    location: 'Greece',
+    postPicture:
+      'https://media.istockphoto.com/id/1500563478/photo/traveler-asian-woman-relax-and-travel-on-thai-longtail-boat-in-ratchaprapha-dam-at-khao-sok.webp?b=1&s=170667a&w=0&k=20&c=PL6F9_KtlUx4E6pl9ryJxR3kLSv-l6aiHAi1DLw84vk=',
+    caption: 'Surfing the sea',
+  },
+  {
+    profilePicture:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxMzkdUV-JQxB68TbKJww27j6Jzaqp5M2gCQ&s',
+    userID: 'gail_ann',
+    location: 'Bacoor, Cavite',
+    postPicture:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTH7YrfUD5rF2O7lUVSCd5VBazlJLRXmAoa-Q&s',
+    caption: 'Me when life is still good',
+  },
+  {
+    profilePicture:
+      'https://cdn.mos.cms.futurecdn.net/p5quSf4dZXctG9WFepXFdR-1200-80.jpg',
+    userID: 'angelie2000',
+    location: 'La Union',
+    postPicture:
+      'https://as1.ftcdn.net/v2/jpg/01/00/82/44/1000_F_100824442_yPbMjMRTsmmpLhdPW3HxAAUc3TM4ll7a.jpg',
+    caption: 'I look cool',
+  },
+  {
+    profilePicture:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEiRHfs79QMz0s9mtHIO615zeBc6SiR76-GA&s',
+    userID: 'aisa-gab',
+    location: 'General Trias, Cavite',
+    postPicture:
+      'https://static.tripzilla.com/thumb/4/e/150094_800x.jpg',
+    caption: 'Cute naman nila',
+  },
+  {
+    profilePicture:
+      'https://i.pinimg.com/736x/1e/57/9f/1e579fe1a9b6bff27e51566236c0c935.jpg',
+    userID: 'Marwin2929',
+    location: 'Tagaytay, Cavite',
+    postPicture:
+      'https://i.pinimg.com/736x/d4/77/82/d477828d8ce84224a5d9e0b4d3749f42.jpg',
+    caption: 'Coffee with cold weather',
+  },
+  {
+    profilePicture:
+      'https://img.freepik.com/free-photo/close-up-young-successful-man-smiling-camera-standing-casual-outfit-against-blue-background_1258-65746.jpg',
+    userID: 'marko_yahoo20',
+    postPicture:
+      'https://images.freeimages.com/variants/mjrbWBMdaBG7WnYHvqQ83M4W/f4a36f6589a0e50e702740b15352bc00e4bfaf6f58bd4db850e167794d05993d?fmt=webp&w=500',
+    caption: 'Unwind',
+  },
+  {
+    profilePicture:
+      'https://img.freepik.com/free-photo/portrait-young-woman-with-natural-make-up_23-2149084942.jpg',
+    userID: 'maryGrace00',
+    location: 'Greece',
+    postPicture:
+      'https://media.istockphoto.com/id/1500563478/photo/traveler-asian-woman-relax-and-travel-on-thai-longtail-boat-in-ratchaprapha-dam-at-khao-sok.webp?b=1&s=170667a&w=0&k=20&c=PL6F9_KtlUx4E6pl9ryJxR3kLSv-l6aiHAi1DLw84vk=',
+    caption: 'Surfing the sea',
+  },
+  {
+    profilePicture:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxMzkdUV-JQxB68TbKJww27j6Jzaqp5M2gCQ&s',
+    userID: 'gail_ann',
+    location: 'Bacoor, Cavite',
+    postPicture:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTH7YrfUD5rF2O7lUVSCd5VBazlJLRXmAoa-Q&s',
+    caption: 'Me when life is still good',
+  },
+  {
+    profilePicture:
+      'https://cdn.mos.cms.futurecdn.net/p5quSf4dZXctG9WFepXFdR-1200-80.jpg',
+    userID: 'angelie2000',
+    location: 'La Union',
+    postPicture:
+      'https://as1.ftcdn.net/v2/jpg/01/00/82/44/1000_F_100824442_yPbMjMRTsmmpLhdPW3HxAAUc3TM4ll7a.jpg',
+    caption: 'I look cool',
+  },
+  {
+    profilePicture:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEiRHfs79QMz0s9mtHIO615zeBc6SiR76-GA&s',
+    userID: 'aisa-gab',
+    location: 'General Trias, Cavite',
+    postPicture:
+      'https://static.tripzilla.com/thumb/4/e/150094_800x.jpg',
+    caption: 'Cute naman nila',
+  },
+  {
+    profilePicture:
+      'https://i.pinimg.com/736x/1e/57/9f/1e579fe1a9b6bff27e51566236c0c935.jpg',
+    userID: 'Marwin2929',
+    location: 'Tagaytay, Cavite',
+    postPicture:
+      'https://i.pinimg.com/736x/d4/77/82/d477828d8ce84224a5d9e0b4d3749f42.jpg',
+    caption: 'Coffee with cold weather',
+  },
+];
