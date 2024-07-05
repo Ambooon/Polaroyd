cada67815da50db23381bc263f88a26d7ba43b6b
Ambooon <angoringfrancis29@gmail.com>
2024-07-05 12:20:35 +0800

    Added Picture Frame component

---
+++ src/components/picture-frame/PictureFrame.tsx
@@ -0,0 +1,36 @@
+import { CiHeart } from 'react-icons/ci';
+
+type PictureFrameProp = {
+  data: {
+    profilePicture: string;
+    userID: string;
+    postPicture: string;
+    caption?: string;
+    location?: string;
+  };
+};
+
+export default function PictureFrame({ data }: PictureFrameProp) {
+  return (
+    <div className='max-w-80 p-4 bg-myNeutral-1'>
+      <div className='flex justify-between items-center mb-2'>
+        <div className='flex items-center gap-2'>
+          <img
+            className='w-10 h-10 object-cover object-center rounded-full'
+            src={data.profilePicture}
+            alt='Profile Picture'
+          />
+          <div className='leading-none'>
+            <p className=''>{data.userID}</p>
+            <p className='text-sm font-thin'>{data.location}</p>
+          </div>
+        </div>
+        <button>
+          <CiHeart size={28} />
+        </button>
+      </div>
+      <img className='mb-4' src={data.postPicture} alt='Post Picture' />
+      <p className='text-center'>{data.caption}</p>
+    </div>
+  );
+}
